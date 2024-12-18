784fef21dae7b6bdb2d9a392e47686b57547d1f5
chenmiao <chenmiao.ku@gmail.com>
2024-12-18 15:35:55 +0000

    done the all exercises

--- exercises/21_macros/macros2.rs
+++ exercises/21_macros/macros2.rs
@@ -1,8 +1,15 @@
+// macro_rules! my_macro {
+//     () => {
+//         println!("Check out my macro!");
+//     };
+// }
+
 fn main() {
     my_macro!();
 }
 
 // TODO: Fix the compiler error by moving the whole definition of this macro.
+#[macro_export]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
